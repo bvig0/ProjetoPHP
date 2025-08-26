30e84535b6f139d4cd61ef86c228e0a9fcc190ac
bvig0 <brian.v.oliveira@gmail.com>
2025-08-26 20:11:22 -0300

    teste

---
+++ funcoes.php
@@ -0,0 +1,23 @@
+<?php 
+
+// criando uma função
+function titulo(){
+	echo("Calculando valores: <br>");
+}
+function soma($num1, $num2){
+	return $num1 + $num2;
+}
+
+
+$valor1 = 10; $valor2 = 20;
+
+//executando a função
+titulo();
+soma($valor1, $valor2);
+$resposta = soma($valor1, $valor2);
+
+echo("A soma de $valor1 e $valor2 é: $resposta <br>");
+
+$somando = soma(30,40) + soma(50, 60);
+echo("<br> $somando")
+ ?>
