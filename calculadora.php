a9a9e2d32bf72e5a9d07563a62c0623780e640fa
bvig0 <brian.v.oliveira@gmail.com>
2025-08-26 22:19:37 -0300

    AAA

--- calculadora.php
+++ calculadora.php
@@ -1,9 +1,9 @@
 <?php 
-	echo ("---CALCULADORA--- <br>");
+	echo ("---CALCULADORA--- <br> selecione uma opção abaixo e efetue a operação <br> 1-Somar <br> 2-subtrair <br> 3-multiplicar <br> 4-dividir ");
 	$valor1 = 10;
 	$valor2 = 20;
 
-	$operador = "+";
+	$operador = "1";
 
 	// funções
 	function somar($num1, $num2){
@@ -19,12 +19,29 @@
 		return($num1 / $num2);
 	}
 
-	if($operador == "+" || $operador == "somar"){
-		echo ("Os números digitados foram: $valor1 e $valor2 <br>Operador escolhido: $operador <br>");
-		$resposta = somar($valor1, $valor2);
-		echo("A conta foi: $valor1 + $valor2 = $resposta <br>");
-
+	function imprimir(){
+		echo ("Os números digitados foram: $valor1 e $valor2 <br>Operador escolhido: $operador <br>A conta foi: $valor1 $operador $valor2 = $resposta <br>");
 	}
+
+	if($operador == "1"){
+			$resposta = somar($valor1, $valor2);
+		}
+		else if($operador == "2"){
+			$operador = "-";
+			$resposta = subtrair($valor1, $valor2);
+		}
+		else if($operador == "3"){
+			$operador = "*";
+			$resposta = multiplicar($valor1, $valor2);
+		}
+		else if($operador == "4"){
+			$operador = "/";
+			$resposta = dividir($valor1, $valor2);
+		}
+		else {
+			echo("Inválido.");
+		}
+	imprimir();
 
 	
  ?>
